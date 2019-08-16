3dbcb750315244cb1c6072e7ebb7748862e9d099
johncpalmer <johnpalmer93@gmail.com>
2019-08-15 21:09:30 -0400

    add better tetrahedron

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ var ColladaLoader = require('three-collada-loader');
 export default class App extends Component {
   componentDidMount() {
     var scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xffffff );
     var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
     var renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -15,7 +16,7 @@ export default class App extends Component {
     var geometry = new THREE.BoxGeometry( 1, 1, 1 );
     var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
     var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    // scene.add( cube );
     var model = new THREE.Mesh(geometry, material);
     var loader = new ColladaLoader();
 
@@ -28,23 +29,21 @@ export default class App extends Component {
       }
     }
 
-    loader.load("model.dae", function (result) {
+    loader.load("tetrahedron.dae", function (result) {
         model = result.scene;
-        setMaterial(model, new THREE.MeshBasicMaterial({color: 0xff0000}));
-        // model.position.x = 0;
-        // model.position.y = 0;
-        // model.position.z = 100;
-        // model.rotation.x = Math.PI / 2;
+        setMaterial(model, new THREE.MeshBasicMaterial({color: 0xacacac}));
+        model.position.x = 0;
+        model.position.y = 0;
+        model.scale.set(.1,.1,.1);
+        model.rotation.x = Math.PI / 2;
         scene.add(model);
     });
     camera.position.z = 5;
 
     var animate = function () {
       requestAnimationFrame( animate );
-      // cube.rotation.x += 0.01;
-      // cube.rotation.y += 0.01;
       model.rotation.x += 0.01;
-      model.rotation.y += 0.01;
+      model.rotation.y += 0.005;
 
       renderer.render( scene, camera );
     };
