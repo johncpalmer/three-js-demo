be76f0d20b89e4a94f62f2b73a994b92cc9e528d
johncpalmer <johnpalmer93@gmail.com>
2019-08-15 20:42:43 -0400

    just got tetrahedra rotating

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import * as THREE from "three";
+var ColladaLoader = require('three-collada-loader');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  componentDidMount() {
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    // document.body.appendChild( renderer.domElement );
+    // use ref as a mount point of the Three.js scene instead of the document.body
+    this.mount.appendChild( renderer.domElement );
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    var model = new THREE.Mesh(geometry, material);
+    var loader = new ColladaLoader();
+
+    var setMaterial = (node, material) => {
+      node.material = material;
+      if (node.children) {
+        for (var i = 0; i < node.children.length; i++) {
+          setMaterial(node.children[i], material);
+        }
+      }
+    }
+
+    loader.load("model.dae", function (result) {
+        model = result.scene;
+        setMaterial(model, new THREE.MeshBasicMaterial({color: 0xff0000}));
+        // model.position.x = 0;
+        // model.position.y = 0;
+        // model.position.z = 100;
+        // model.rotation.x = Math.PI / 2;
+        scene.add(model);
+    });
+    camera.position.z = 5;
+
+    var animate = function () {
+      requestAnimationFrame( animate );
+      // cube.rotation.x += 0.01;
+      // cube.rotation.y += 0.01;
+      model.rotation.x += 0.01;
+      model.rotation.y += 0.01;
+
+      renderer.render( scene, camera );
+    };
+    animate();
+  }
+
+  render() {
+    return (
+      <div ref={ref => (this.mount = ref)} />
+    )
+  }
 }
-
-export default App;
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
